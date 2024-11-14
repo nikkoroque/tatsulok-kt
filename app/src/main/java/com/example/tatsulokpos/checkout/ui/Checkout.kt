23301300bd5c26133d8b36827b2198160118ffc3
nikkoroque Roque <106610608+nikkoroque@users.noreply.github.com>
2024-11-13 20:34:01 -0800

    hotfix/checkout payment options

--- app/src/main/java/com/example/tatsulokpos/checkout/ui/Checkout.kt
+++ app/src/main/java/com/example/tatsulokpos/checkout/ui/Checkout.kt
@@ -1,32 +1,26 @@
 package com.example.tatsulokpos.checkout.ui
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.shape.CutCornerShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.RadioButton
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -34,10 +28,15 @@ import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import coil.compose.AsyncImage
-import com.example.tatsulokpos.layout.CartDisplay
-import com.example.tatsulokpos.product.model.CartItemModel
 import com.example.tatsulokpos.utils.currencyFormatter
-import org.w3c.dom.Text
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.height
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
 
 
 @Composable
@@ -54,37 +53,12 @@ fun CheckOutSection(price : Double) {
     }
 }
 
-//@Composable
-//fun CheckOutButton(){
-//    var showDialog by remember{ mutableStateOf(false)}
-//    var showCheckOutMessage by remember { mutableStateOf(false) }
-//    Box(Modifier.fillMaxWidth()){
-//        Button(onClick = { showDialog = true}, modifier = Modifier.align(Alignment.BottomStart).fillMaxWidth(), shape = RoundedCornerShape(12.dp), colors = ButtonDefaults.buttonColors(containerColor = Color.Black, contentColor = Color.White)) {
-//            Text(text = "Checkout")
-//        }
-//    }
-//    // Show the dialog conditionally
-//    if (showDialog) {
-//   PaymentOptionsDialog(showDialog = showDialog,
-//       onDismiss = { showDialog = false },  // Dismiss the dialog
-//       onConfirm = {
-//           showDialog = false
-//           showCheckOutMessage = true;
-//           // You can also add more logic for handling the checkout here
-//       })
-//    }
-//
-//    if(showCheckOutMessage){
-//        CheckOutMessage( onDismiss = { showCheckOutMessage = false })
-//    }
-//
-//
-//}
-
 @Composable
 fun CheckOutButton(onCheckoutComplete: () -> Unit) {
     var showDialog by remember { mutableStateOf(false) }
     var showCheckOutMessage by remember { mutableStateOf(false) }
+    var isProcessing by remember { mutableStateOf(false) }
+    val coroutineScope = rememberCoroutineScope()
 
     Box(Modifier.fillMaxWidth()) {
         Button(
@@ -105,16 +79,36 @@ fun CheckOutButton(onCheckoutComplete: () -> Unit) {
             onDismiss = { showDialog = false },
             onConfirm = {
                 showDialog = false
-                showCheckOutMessage = true
-                onCheckoutComplete() // Clear cart items after checkout
+                isProcessing = true
+                coroutineScope.launch {
+                    delay(2000) // Simulate payment processing delay
+                    isProcessing = false
+                    showCheckOutMessage = true
+                    onCheckoutComplete() // Clear cart items after checkout
+                }
             }
         )
+    }
+
+    if (isProcessing) {
+        ProcessingMessage()
     }
 
     if (showCheckOutMessage) {
         CheckOutMessage(onDismiss = { showCheckOutMessage = false })
     }
 }
+
+@Composable
+fun ProcessingMessage() {
+    AlertDialog(
+        onDismissRequest = {},
+        title = null,
+        text = { Text("Simulating Payment Process...") },
+        confirmButton = {}
+    )
+}
+
 
 @Composable
 fun PaymentOptionsDialog(
@@ -126,26 +120,27 @@ fun PaymentOptionsDialog(
         var selectedOption by remember { mutableStateOf("") }
         AlertDialog(
             onDismissRequest = { onDismiss() },
-            title = { Text(text = "Choose Payment Option") },
+            title = { Text(text = "Choose Payment Option", fontWeight = FontWeight.Bold) },
             text = {
                 Column {
-                    // Each option displays an image instead of text
                     PaymentOptionRow(
-                        option = "Gcash",
+                        option = "Creditcard/Non-Creditcard Installment Payments",
                         selectedModeOfPayment = selectedOption,
-                        imageUrl = "https://media.licdn.com/dms/image/C5612AQGaOlwdWwWOyg/article-cover_image-shrink_600_2000/0/1610443196243?e=2147483647&v=beta&t=tDEZDdlZ7VDvyK2bXoZvwuzcnJ39MjzpwOd3e1pF5_o", // Replace with actual image URL or resource
+                        imageUrl = "https://i.imgur.com/EIqzXLb.png",
                         onSelect = { selectedOption = it }
                     )
+                    Spacer(modifier = Modifier.height(16.dp))
                     PaymentOptionRow(
-                        option = "Cash",
+                        option = "Credit/Debit Card Payments",
                         selectedModeOfPayment = selectedOption,
-                        imageUrl = "https://media.istockphoto.com/id/1270169656/photo/paying-and-receiving-of-philippine-peso-bills-on-white-background.jpg?s=612x612&w=0&k=20&c=3Rc2GTowUuoJuCNGeRjD4u4l26Fi0MkQUcGRuI2flYo=", // Replace with actual image URL or resource
+                        imageUrl = "https://i.imgur.com/PnUYhLr.png",
                         onSelect = { selectedOption = it }
                     )
+                    Spacer(modifier = Modifier.height(16.dp))
                     PaymentOptionRow(
-                        option = "Debit or Credit Card",
+                        option = "eWallet Payments",
                         selectedModeOfPayment = selectedOption,
-                        imageUrl = "https://streetfins.com/wp-content/uploads/2020/12/debt-vs-credit-11.jpg", // Replace with actual image URL or resource
+                        imageUrl = "https://i.imgur.com/EIX5Ooo.png",
                         onSelect = { selectedOption = it }
                     )
                 }
@@ -167,7 +162,6 @@ fun PaymentOptionsDialog(
     }
 }
 
-
 @Composable
 fun PaymentOptionRow(
     option: String,
@@ -175,27 +169,33 @@ fun PaymentOptionRow(
     imageUrl: String, // URL or resource ID for the image
     onSelect: (String) -> Unit
 ) {
-    Row(
-        verticalAlignment = Alignment.CenterVertically,
+    val isSelected = selectedModeOfPayment == option
+    Column(
+        verticalArrangement = Arrangement.Center,
         modifier = Modifier
             .fillMaxWidth()
+            .border(
+                width = if (isSelected) 2.dp else 1.dp,
+                color = if (isSelected) Color.Black else Color.Gray,
+                shape = RoundedCornerShape(8.dp)
+            )
             .clickable { onSelect(option) } // Make the entire row clickable
-            .padding(vertical = 8.dp)
+            .padding(8.dp)
     ) {
         // Load the image for the payment option
+        // Display the option text
+        Text(option, modifier = Modifier.align(Alignment.Start), fontSize = 18.sp, fontWeight = FontWeight.Bold)
+
+        Spacer(modifier = Modifier.width(8.dp))
+
         AsyncImage(
             model = imageUrl,
             contentDescription = option,
             modifier = Modifier
-                .size(80.dp) // Adjust size as needed
-                .clip(RoundedCornerShape(4.dp)) // Optional: round the corners of the image
-                .padding(4.dp) // Optional: add some padding around the image
+                .clip(RoundedCornerShape(4.dp))
+                .padding(4.dp)
         )
 
-        Spacer(modifier = Modifier.width(8.dp))
-
-        // Display the option text, optional if you only want the images
-        Text(option, modifier = Modifier.align(Alignment.CenterVertically))
     }
 }
 
