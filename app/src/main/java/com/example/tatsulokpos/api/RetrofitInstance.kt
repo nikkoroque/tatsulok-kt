1ec44bdb6f33b1659b61c75803b87fa6a1465930
SidJTolentino <sidjustin.tolentino@gmail.com>
2025-04-03 08:00:14 +0800

    Fixed Products
    - Disable product card when quantity is 0

--- app/src/main/java/com/example/tatsulokpos/api/RetrofitInstance.kt
+++ app/src/main/java/com/example/tatsulokpos/api/RetrofitInstance.kt
@@ -7,7 +7,7 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitInstance {
-    private const val BASE_URL = "http://10.0.2.2:8000" // Use 10.0.2.2 for localhost in Android emulator
+    private const val BASE_URL = "https://tatsulok.cloud/api/" // Use 10.0.2.2 for localhost in Android emulator
 
     private val retrofit: Retrofit by lazy {
         Retrofit.Builder()
